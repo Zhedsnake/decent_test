c2fd6adf6a76f86f07d7e09374311c5674d03d67
Zhedsnake <besedinzena5@gmail.com>
2024-07-31 16:47:27 +0300

    start

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+
+const App: React.FC  = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default App ;
